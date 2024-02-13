a3210527c871ce4da629a1454848a72636c550d1
Ahmed <Ahmed.elmghari502@gmail.com>
2024-02-12 21:10:24 -0500

    hello my friend

--- snakemain.py
+++ snakemain.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,32 +8,20 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0,0), (-20,0), (-40,0)]
+snake = Snake()
 
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square") # 20x20  
-    new_segment.color("white") # 
-    new_segment.penup() #  don't draw lines
-    new_segment.goto(position) #   move to position
-    segments.append(new_segment) #  add to list
+screen.listen()
+screen.onkey(snake.up,"W")
+screen.onkey(snake.down, "S")
+screen.onkey(snake.left, "A")
+screen.onkey(snake.right, "D")
 
 game_is_on = True
 while game_is_on:
-    screen.update() # update the screen with the new positions of the segments i.e. the snake body so x and y coordinates
-    #  
+    screen.update()
     time.sleep(0.1)
 
-    for seg_num in range( len(segments)-1, 0,-1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y) 
-    segments[0].forward(20)
-    
-     
-    
-
+    snake.move()
 
 
 
