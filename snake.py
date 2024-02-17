406b65face9f04d40607942bcba953456ce40442
Ahmed <Ahmed.elmghari502@gmail.com>
2024-02-17 11:20:12 -0500

    snake game day 21

--- snake.py
+++ snake.py
@@ -15,18 +15,24 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
             new_segment.goto(position)
             self.segments.append(new_segment)
 
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.head.segments[0].forward(MOVE_DISTANCE)
+            self.head.segments[0].forward(MOVE_DISTANCE)
 
     
     def up(self):
