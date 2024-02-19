19778ea82423d324fd34bbf38cdc080fb0950552
Ahmed <Ahmed.elmghari502@gmail.com>
2024-02-19 18:14:58 -0500

    ping pong game

--- food.py
+++ food.py
@@ -7,10 +7,10 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.5, streth_width=0.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        self.resfresh()
+        self.refresh()
 
     def refresh(self):
         random_x = random.randint(-280, 280)
